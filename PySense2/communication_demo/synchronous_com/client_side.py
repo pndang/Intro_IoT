60c5848754db6adb901ab510c307dda1a7d6cfab
Phu Dang <dangnphu31@gmail.com>
2023-07-26 05:38:55 -0700

    add synchronous com testing code

--- PySense2/communication_demo/synchronous_com/client_side.py
+++ PySense2/communication_demo/synchronous_com/client_side.py
@@ -1,45 +1,116 @@
 import time
 from network import Bluetooth
 
-SERVICE_UUID = 0xAAAA
-DATA_CHARACTERISTIC_UUID = 0xBBBB
-ACK_CHARACTERISTIC_UUID = 0xCCCC
+SERVICE_UUID = 0x1800
+DATA_CHARACTERISTIC_UUID = 0x2A00
+ACK_CHARACTERISTIC_UUID = 0x2A01
 
-# bt = Bluetooth()
-# bt.set_advertisement(name='Client', service_uuid=SERVICE_UUID)
+SERVER_NAME = 'Grp3'  # Replace 'Server' with the actual name of the server device
 
-def handle_data(chr):
-    value = chr.value()
-    print("Received:", value)
+bluetooth = Bluetooth()
 
-def send_acknowledgment(chr):
-    chr.value('ACK')  # Send acknowledgment message
+def handle_data(chr, event_data):
+    time.sleep(0.1)
+    print('in handle')
+    if bluetooth.isconnected():
+        print('is connected')
+        data = chr.value()
+        print("New value: {}".format(data))
 
-# bt.advertise(True)
+        ack_chr.value('ACK')  # Send acknowledgment message
+        print('sent ack')
 
-bt = Bluetooth()
-print('Start scanning for BLE services')
-bt.start_scan(-1)
-adv = None
+    else:
+        print("Not connected.")
 
+print("Scanning for servers...")
+bluetooth.start_scan(-1)  # Scan indefinitely until connection is established
+
+# Scanning and Connection Loop
 while True:
-    adv = bt.get_adv()
-    if adv and bt.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL)=="Team 3: Aidan & Phu":
-        conn = bt.connect(adv.mac)
-        if conn:
-            print("Connected to Team 3: Aidan & Phu")
+    advs = bluetooth.get_advertisements()
+    for adv in advs:
+        name = bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL)
+        if name == SERVER_NAME:
+            conn = bluetooth.connect(adv.mac)
+            print("Connected to Grp3")
             try:
-                service = conn.services()[0]
-                data_chr = service.characteristic(DATA_CHARACTERISTIC_UUID)
-                ack_chr = service.characteristic(ACK_CHARACTERISTIC_UUID)
+                services = conn.services()
+                for service in services:
+                    characteristics = service.characteristics()
+                    for characteristic in characteristics:
+                        if characteristic.uuid() == DATA_CHARACTERISTIC_UUID:
+                            data_chr = characteristic
+                            print(data_chr.properties())
+                            print(Bluetooth.PROP_NOTIFY)
+                            if (data_chr.properties() & Bluetooth.PROP_NOTIFY):
+                                print('in data')
+                                data_chr.callback(trigger=Bluetooth.CHAR_NOTIFY_EVENT, handler=handle_data)
+                        elif characteristic.uuid() == ACK_CHARACTERISTIC_UUID:
+                            print('in ack')
+                            ack_chr = characteristic
 
-                conn.subscribe(data_chr, callback=handle_data)
+                print('test')
 
-                while conn.isconnected():
-                    send_acknowledgment(ack_chr)
-                    time.sleep(2)
+                if (data_chr.properties() & Bluetooth.PROP_NOTIFY):
+                    while conn.isconnected():
+                        print(data_chr.value())
+                        time.sleep(2)
+
             except:
                 continue
+            
 
-bt.stop_scan()
-bt.disconnect_client() 
+# def send_acknowledgment(chr):
+#     chr.write('ACK')  # Send acknowledgment message
+
+
+
+
+# *****
+
+# import time
+# from network import Bluetooth
+
+# SERVICE_UUID = 0xAAAA
+# DATA_CHARACTERISTIC_UUID = 0xBBBB
+# ACK_CHARACTERISTIC_UUID = 0xCCCC
+
+# # bt = Bluetooth()
+# # bt.set_advertisement(name='Client', service_uuid=SERVICE_UUID)
+
+# def handle_data(chr):
+#     value = chr.value()
+#     print("Received:", value)
+
+# def send_acknowledgment(chr):
+#     chr.value('ACK')  # Send acknowledgment message
+
+# # bt.advertise(True)
+
+# bt = Bluetooth()
+# print('Start scanning for BLE services')
+# bt.start_scan(-1)
+# adv = None
+
+# while True:
+#     adv = bt.get_adv()
+#     if adv and bt.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL)=="Team 3: Aidan & Phu":
+#         conn = bt.connect(adv.mac)
+#         if conn:
+#             print("Connected to Team 3: Aidan & Phu")
+#             try:
+#                 service = conn.services()[0]
+#                 data_chr = service.characteristic(DATA_CHARACTERISTIC_UUID)
+#                 ack_chr = service.characteristic(ACK_CHARACTERISTIC_UUID)
+
+#                 conn.subscribe(data_chr, callback=handle_data)
+
+#                 while conn.isconnected():
+#                     send_acknowledgment(ack_chr)
+#                     time.sleep(2)
+#             except:
+#                 continue
+
+# bt.stop_scan()
+# bt.disconnect_client() 
--- PySense2/communication_demo/synchronous_com/server_side.py
+++ PySense2/communication_demo/synchronous_com/server_side.py
@@ -1,41 +1,131 @@
 import time
 from network import Bluetooth
 
-SERVICE_UUID = 0xAAAA
-DATA_CHARACTERISTIC_UUID = 0xBBBB
-ACK_CHARACTERISTIC_UUID = 0xCCCC
+SERVICE_UUID = 0x1800
+DATA_CHARACTERISTIC_UUID = 0x2A00
+ACK_CHARACTERISTIC_UUID = 0x2A01
 
-bt = Bluetooth()
-bt.set_advertisement(name='Team 3: Aidan & Phu', service_uuid=SERVICE_UUID)
+bluetooth = Bluetooth()
+bluetooth.set_advertisement(name='Grp3', service_uuid=SERVICE_UUID)
 
-srv = bt.service(uuid=SERVICE_UUID, isprimary=True)
-data_chr = srv.characteristic(uuid=DATA_CHARACTERISTIC_UUID, value='Hello, Client!', properties=Bluetooth.PROP_NOTIFY)
-ack_chr = srv.characteristic(uuid=ACK_CHARACTERISTIC_UUID, value='Waiting for ACK', properties=Bluetooth.PROP_READ)
+srv = bluetooth.service(uuid=SERVICE_UUID, isprimary=True)
+data_chr = srv.characteristic(uuid=DATA_CHARACTERISTIC_UUID, value='Hello, Client!', properties=Bluetooth.PROP_READ | Bluetooth.PROP_NOTIFY)
+ack_chr = srv.characteristic(uuid=ACK_CHARACTERISTIC_UUID, value='Waiting for ACK', properties=Bluetooth.PROP_WRITE)
 
 # Flag to indicate acknowledgment status
 acknowledgment_received = False
 
+# Flag to indicate client connection status
+client_connected = False
+first_packet = True
+
 def send_data():
-    data_chr.notify(True)
-    data_chr.value('Hello, Client!')
+    global acknowledgment_received, client_connected
+    if acknowledgment_received and client_connected:
+        # Send data to the client using notifications
+        data_chr.value('Hello, Client!')
+        acknowledgment_received = False  # Set flag to False, waiting for acknowledgment
+        time.sleep(2)
 
-def handle_ack(chr):
+def first_send():
+    global first_packet, client_connected
+    print('in first send')
+    if client_connected:
+        print('in send')
+        data_chr.value('Hello!!')
+        first_packet = False
+        print('first sent')
+        print(data_chr.value())
+        time.sleep(2)
+
+def handle_ack(chr, data):
     global acknowledgment_received
-    value = chr.value()
-    print("ACK Received:", value)
-    acknowledgment_received = True
+    print("ACK Received:", data)
+    acknowledgment_received = True  # Set flag to True, acknowledgment received
 
-ack_chr.callback(trigger=Bluetooth.CHAR_NOTIFY_EVENT, handler=handle_ack)
+ack_chr.callback(trigger=Bluetooth.CHAR_WRITE_EVENT, handler=handle_ack)
 
-bt.advertise(True)
+def conn_cb(chr):
+    global client_connected
+    events = chr.events()
+    if events & Bluetooth.CLIENT_CONNECTED:
+        print('client connected')
+        client_connected = True
+    elif events & Bluetooth.CLIENT_DISCONNECTED:
+        print('client disconnected')
+        client_connected = False
+
+bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
+
+print("BLE Started!")
+bluetooth.advertise(True)
 
 while True:
-    # Wait until acknowledgment is received
-    while not acknowledgment_received:
-        pass  # Do nothing and keep waiting
 
-    # Acknowledgment received, reset the flag and send data
-    acknowledgment_received = False
+    print('test')
+
+    print(first_packet)
+
+    print("ACK: {}".format(acknowledgment_received))
+    
+    if client_connected:
+        while not acknowledgment_received:
+            data_chr.value('hello!!! from loop')
+            print(data_chr.value())
+            time.sleep(2)
+
+    # if first_packet:
+    #     first_send()
+
     send_data()
-    time.sleep(5)
+    time.sleep(2)
+
+
+
+
+
+
+# *****
+
+
+# import time
+# from network import Bluetooth
+
+# SERVICE_UUID = 0xAAAA
+# DATA_CHARACTERISTIC_UUID = 0xBBBB
+# ACK_CHARACTERISTIC_UUID = 0xCCCC
+
+# bt = Bluetooth()
+# bt.set_advertisement(name='Team 3: Aidan & Phu', service_uuid=SERVICE_UUID)
+
+# srv = bt.service(uuid=SERVICE_UUID, isprimary=True)
+# data_chr = srv.characteristic(uuid=DATA_CHARACTERISTIC_UUID, value='Hello, Client!', properties=Bluetooth.PROP_NOTIFY)
+# ack_chr = srv.characteristic(uuid=ACK_CHARACTERISTIC_UUID, value='Waiting for ACK', properties=Bluetooth.PROP_READ)
+
+# # Flag to indicate acknowledgment status
+# acknowledgment_received = False
+
+# def send_data():
+#     data_chr.notify(True)
+#     data_chr.value('Hello, Client!')
+
+# def handle_ack(chr):
+#     global acknowledgment_received
+#     value = chr.value()
+#     print("ACK Received:", value)
+#     acknowledgment_received = True
+
+# ack_chr.callback(trigger=Bluetooth.CHAR_NOTIFY_EVENT, handler=handle_ack)
+
+# bt.advertise(True)
+
+# while True:
+#     # Wait until acknowledgment is received
+#     while not acknowledgment_received:
+#         pass  # Do nothing and keep waiting
+
+#     # Acknowledgment received, reset the flag and send data
+#     acknowledgment_received = False
+#     send_data()
+#     time.sleep(5)
     
