aadec396c059b611ef5d95b358afcebb3845ea58
Phu Dang <dangnphu31@gmail.com>
2023-07-28 05:35:38 -0700

    add wifi program

---
+++ Project_SL2/WiFi_Connection/personal_hotspot_demo/personal_hotspot_demo.ino
@@ -0,0 +1,108 @@
+/*
+  Source from Rui Santos - Implemented/modified for project purposes by Phu Dang
+
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+*/
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include <Wire.h>
+
+// Replace with your network credentials
+const char* ssid = "Phu Dang";
+const char* password = "dangphu9220";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "https://UCSD-HDSI-IOT.com/post-esp-data.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+String sensorName = "PIR_Sensor";
+String sensorLocation = "Makerspace";
+
+int irPin = 4;
+int ledPin = 2;
+int irVal = 0;
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(irPin, INPUT);
+}
+
+void loop() {
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    WiFiClientSecure *client = new WiFiClientSecure;
+    client->setInsecure(); //don't use SSL certificate
+    HTTPClient https;
+    
+    // Your Domain name with URL path or IP address with path
+    https.begin(*client, serverName);
+    
+    // Specify content-type header
+    https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // Prepare your HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                          + "&location=" + sensorLocation + "&value1=" + String(digitalRead(irPin))
+                          + "&value2=" + String(digitalRead(irPin)) + "&value3=" + String(digitalRead(irPin)) + "";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = https.POST(httpRequestData);
+     
+    // If you need an HTTP request with a content type: text/plain
+    //https.addHeader("Content-Type", "text/plain");
+    //int httpResponseCode = https.POST("Hello, World!");
+    
+    // If you need an HTTP request with a content type: application/json, use the following:
+    //https.addHeader("Content-Type", "application/json");
+    //int httpResponseCode = https.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+    
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      digitalWrite(ledPin, HIGH);
+      delay(2000);
+      digitalWrite(ledPin, LOW);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    https.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 30 seconds
+  delay(11000);  
+}
